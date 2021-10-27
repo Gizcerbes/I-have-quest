482e763d05bfd31b77e8b7256b3471dd1ba27899
gizcerbes@outlook.com <278152andr3328>
2021-10-27 17:41:29 +0300

    Update LoginViewModel.kt

--- app/src/main/java/com/uogames/i_have_quest/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/uogames/i_have_quest/ui/login/LoginViewModel.kt
@@ -8,13 +8,52 @@ import javax.inject.Inject
 
 class LoginViewModel @Inject constructor(private val game: GameProvider) : ViewModel() {
 
+    enum class Errors {
+        OK, SHORT, LONG
+    }
+
+
     private val _username = MutableLiveData("")
     val username: LiveData<String> = _username
+    private val _usernameError = MutableLiveData(Errors.OK)
+    val usernameError: LiveData<Errors> = _usernameError
 
+    private val _password = MutableLiveData("")
+    val password: LiveData<String> = _password
+    private val _passwordError = MutableLiveData(Errors.OK)
+    val passwordError: LiveData<Errors> = _passwordError
 
+    private val _busy = MutableLiveData(false)
+    val busy: LiveData<Boolean> = _busy
 
     fun setUsername(username: String) {
         _username.value = username
+    }
+
+    fun setPassword(password: String) {
+        _password.value = password
+    }
+
+    fun login(result: (Boolean) -> Unit = {}) {
+        val username = username.value
+        val password = password.value
+
+        if (username.isNullOrEmpty() || username.length < 3) {
+            _usernameError.value = Errors.SHORT
+        }
+
+        if (password.isNullOrEmpty() || password.length < 5) {
+            _passwordError.value = Errors.SHORT
+        }
+
+        if (
+            usernameError.value == Errors.OK &&
+            passwordError.value == Errors.OK
+        ) {
+            game.login(username.orEmpty(), password.orEmpty(), result)
+        }
+
+
     }
 
 }
